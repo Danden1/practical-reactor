05e2ebf601218473afe0883b0700a5044063355c
Yongha Kwon <yongha.kwon12@gmail.com>
2023-07-13 14:58:30 +0900

    Update c4_LifecycleHooks.java

--- exercises/src/test/java/c4_LifecycleHooks.java
+++ exercises/src/test/java/c4_LifecycleHooks.java
@@ -36,10 +36,9 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
     public void no_subscription_no_gains() {
         CopyOnWriteArrayList<String> hooksTriggered = new CopyOnWriteArrayList<>();
 
-        Flux<Integer> temperatureFlux = room_temperature_service()
+        Flux<Integer> temperatureFlux = room_temperature_service().doOnSubscribe(i->hooksTriggered.add("subscribe"))
                 //todo: change this line only
                 ;
-
         StepVerifier.create(temperatureFlux.take(5))
                     .expectNextCount(5)
                     .verifyComplete();
@@ -55,9 +54,15 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
     public void be_there_early() {
         CopyOnWriteArrayList<String> hooksTriggered = new CopyOnWriteArrayList<>();
 
-        Flux<Integer> temperatureFlux = room_temperature_service()
+        //구독하기 전에 한번 실행함.
+        Flux<Integer> temperatureFlux = room_temperature_service().doFirst(() -> hooksTriggered.add("before subscribe"))
                 //todo: change this line only
                 ;
+
+        //만약 subscriber가 하나 더 추가되면?
+        //temperatureFlux.subscribe(s -> System.out.println(s));
+        //"before subscribe"가 하나 더 들어감. 총 2개가 됨.
+        //근데 subscribe는 왜 하나지?
 
         StepVerifier.create(temperatureFlux.take(5).doOnSubscribe(s -> hooksTriggered.add("subscribe")))
                     .expectNextCount(5)
@@ -75,6 +80,10 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
         AtomicInteger counter = new AtomicInteger(0);
 
         Flux<Integer> temperatureFlux = room_temperature_service()
+                .doOnNext(s -> {
+                    System.out.println(counter.get());
+                    counter.updateAndGet(a -> a + 1);
+                })
                 //todo: change this line only
                 ;
 
@@ -82,6 +91,8 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
                     .expectNextCount(20)
                     .verifyComplete();
 
+        //순서 반대 아닌가?
+        //https://github.com/schananas/practical-reactor/issues/21 등록함.
         Assertions.assertEquals(counter.get(), 20);
     }
 
@@ -93,13 +104,18 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
     public void successfully_executed() {
         AtomicBoolean completed = new AtomicBoolean(false);
 
-        Flux<Integer> temperatureFlux = room_temperature_service()
+        Flux<Integer> temperatureFlux = room_temperature_service().doOnComplete(() -> completed.set(!completed.get()));
                 //todo: change this line only
                 ;
 
         StepVerifier.create(temperatureFlux.skip(20))
                     .expectNextCount(0)
                     .verifyComplete();
+
+//        StepVerifier.create(temperatureFlux.skip(20))
+//                .expectNextCount(0)
+//                .verifyComplete();
+//        false가 됨. 물론 그냥 true로 해야함.
 
         Assertions.assertTrue(completed.get());
     }
@@ -112,7 +128,7 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
     public void need_to_cancel() {
         AtomicBoolean canceled = new AtomicBoolean(false);
 
-        Flux<Integer> temperatureFlux = room_temperature_service()
+        Flux<Integer> temperatureFlux = room_temperature_service().doOnCancel(() -> canceled.set(true));
                 //todo: change this line only
                 ;
 
@@ -132,7 +148,7 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
     public void terminator() {
         AtomicInteger hooksTriggeredCounter = new AtomicInteger(0);
 
-        Flux<Integer> temperatureFlux = room_temperature_service()
+        Flux<Integer> temperatureFlux = room_temperature_service().doOnTerminate(hooksTriggeredCounter::getAndIncrement)
                 //todo: change this line only
                 ;
 
@@ -160,9 +176,12 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
     public void one_to_catch_them_all() {
         AtomicInteger hooksTriggeredCounter = new AtomicInteger(0);
 
-        Flux<Integer> temperatureFlux = room_temperature_service()
+        Flux<Integer> temperatureFlux = room_temperature_service().doFinally(s -> {
+            System.out.println(s);
+            hooksTriggeredCounter.getAndIncrement();
+        });
                 //todo: change this line only
-                ;
+        //finally는 인자로 상태값이 들어옴.
 
         StepVerifier.create(temperatureFlux.take(0))
                     .expectNextCount(0)
@@ -193,7 +212,7 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
                                  .doFirst(() -> sideEffects.add("one"));
 
         List<String> orderOfExecution =
-                Arrays.asList("todo", "todo", "todo"); //todo: change this line only
+                Arrays.asList("one", "two", "three"); //todo: change this line only
 
         StepVerifier.create(just)
                     .expectNext(true)
@@ -216,14 +235,18 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
     public void one_to_rule_them_all() {
         CopyOnWriteArrayList<String> signals = new CopyOnWriteArrayList<>();
 
-        Flux<Integer> flux = Flux.just(1, 2, 3)
+        Flux<Integer> flux = Flux.just(1, 2, 3).doOnEach(s -> signals.add(s.getType().name()))
                 //todo: change this line only
+                //그냥 s.toString() 하면 안됨.
+                // getType.toString()도 안됨. 소문자라서
                 ;
 
         StepVerifier.create(flux)
                     .expectNextCount(3)
                     .verifyComplete();
 
+        
+        //이것도 거꾸로 되어 있음.
         Assertions.assertEquals(signals, Arrays.asList("ON_NEXT", "ON_NEXT", "ON_NEXT", "ON_COMPLETE"));
     }
 }
